acc08ecc908d19c580752870deffce8b7e70da67
JENOSS <nari1539@gmail.com>
2024-03-07 23:55:30 +0900

    initial

---
+++ src/main/java/com/app/compartz/ComPartzApplication.java
@@ -0,0 +1,13 @@
+package com.app.compartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComPartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComPartzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/compartz/ComPartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.compartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComPartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
