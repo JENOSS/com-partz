be33a3f9dc8783fd2ec2a7690326f8877a267029
JENOSS <nari1539@gmail.com>
2024-03-10 22:49:58 +0900

    [DEV-3] Exception 처리
     - 토큰 관련 메세지 처리 수정

--- src/main/java/com/app/compartz/component/security/JwtAccessDeniedHandler.java
+++ src/main/java/com/app/compartz/component/security/JwtAccessDeniedHandler.java
@@ -1,4 +1,5 @@
 package com.app.compartz.component.security;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
@@ -14,7 +16,10 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         // 필요한 권한이 없이 접근하려 할때 403
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
-
+        response.setHeader("Content-Type", "application/json; charset=UTF-8");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
+        writer.write("권한이 없습니다.");
+        writer.close();
     }
 }
--- src/main/java/com/app/compartz/component/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/app/compartz/component/security/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.app.compartz.component.security;
 
+import com.app.compartz.component.exception.CustomException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Objects;
 
 
 @Component
@@ -15,6 +19,10 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("Content-Type", "application/json; charset=UTF-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
+        writer.write("유효하지않은 토큰 정보입니다.");
+        writer.close();
     }
 }
